25c45f2198ac9f0d23bad285c04a6abcecec4789
Sombit Pramanik <m.sombitpramanik@gmail.com>
2024-01-06 10:16:14 +0530

    concept of size_t and other formate spiceifeare

---
+++ day3/formatespicifeare.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int num = 42;
+    double pi = 3.14159;
+    char ch = 'A';
+    char str[] = "Hello, World!";
+
+    printf("Integer: %d\n", num);
+    printf("Floating-point: %.2f\n", pi);
+    printf("Character: %c\n", ch);
+    printf("String: %s\n", str);
+
+    return 0;
+}
