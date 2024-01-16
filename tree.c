3e04b6d2b8fcbf2470245142dd06d70568d03512
Sombit Pramanik <m.sombitpramanik@gmail.com>
2024-01-17 04:04:50 +0530

    add the user input inserssion and searching

--- tree.c
+++ tree.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 struct tree
 {
@@ -38,6 +39,28 @@ struct tree *insert(struct tree *root, int value)
 
     return root;
 }
+bool search(struct tree *root, int value)
+{
+    if (root != NULL)
+    {
+        if (root->data == value)
+        {
+            return true;
+        }
+        else if (value < root->data)
+        {
+            return search(root->left, value);
+        }
+        else
+        {
+            return search(root->right, value);
+        }
+    }
+    else
+    {
+        return false;
+    }
+}
 void inOrderTraversal(struct tree *root)
 {
     if (root != NULL)
@@ -51,16 +74,34 @@ void inOrderTraversal(struct tree *root)
 int main()
 {
     struct tree *BTree = NULL;
-    int values[] = {1, 2, 3, 4, 5};
+    int values[10];
+    int key;
+    printf("Enter value to be inserted in the Tree max-5: \n");
+    for(int i=0;i<5;i++){
+        printf("\nEnter Value No %d: ",i+1);
+        if(scanf("%d",&values[i])){
+            BTree = insert(BTree,values[i]);
+        }else{
+            printf("\nInvalid Input\n");
+            break;
+        }
 
-    for (int i = 0; i < sizeof(values) / sizeof(values[0]); ++i) {
-        BTree = insert(BTree, values[i]);
     }
     printf("In-order traversal: ");
     inOrderTraversal(BTree);
     printf("\n");
+    printf("Enter the Key you want to search : ");
+    scanf("%d", &key);
+    if (search(BTree, key))
+    {
+        printf("\nvalue : %d Found in the Tree.\n", key);
+    }
+    else
+    {
+        printf("Value Not Found in the Tree\n");
+    }
 
     free(BTree);
-    
+
     return 0;
 }
